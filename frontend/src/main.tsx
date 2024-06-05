4d6225adf121182a49778f1ac5779ed742ef07cb
rigolao <mrigolao@gmail.com>
2024-06-04 23:35:36 -0300

    Feat: Adicionando Provider do React Query.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {ThemeProvider} from "@/providers/theme-provider.tsx";
 import {AlertDialogProvider} from "@/providers/alert-dialog-provider.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {Toaster} from "sonner";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <QueryClientProvider client={queryClient}>
             <AlertDialogProvider>
                 <Toaster richColors closeButton />
                 <App/>
             </AlertDialogProvider>
-        </ThemeProvider>
-    </React.StrictMode>,
+        </QueryClientProvider>
+    </ThemeProvider>
 )
