aecb126ecc4e82d8c5cb1a2336a08c1229674669
jomoliveira <jomoliveira@unaerp.br>
2024-07-03 09:15:34 -0300

    feat: estrutura inicial do projeto

---
+++ backend/src/test/java/br/socialhub/api/SocialhubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.socialhub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
