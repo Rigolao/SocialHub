30089e590956b0d3d82d627dd1215a60efc9c9a1
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-19 10:00:42 -0300

    feat: mapeamento da entidade postagem

---
+++ backend/src/main/java/br/socialhub/api/models/Postagem.java
@@ -0,0 +1,36 @@
+package br.socialhub.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "POSTAGEM", schema = "socialhub")
+@Entity
+public class Postagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IDPOSTAGEM")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "IDPARTICIPANTESPACE")
+    private ParticipanteSpace participanteSpace;
+
+    @Column(name = "DESCRICAO", nullable = false)
+    private String descricao;
+
+    @Column(name = "DATAAGENDAMENTO", nullable = false)
+    private LocalDateTime dataAgendamento;
+
+    @OneToMany(mappedBy = "postagem", fetch = FetchType.LAZY)
+    private List<Anexo> anexos;
+}
