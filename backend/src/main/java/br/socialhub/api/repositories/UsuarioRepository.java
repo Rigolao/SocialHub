ce965c229d4aa3be61b435eb18a32c68b6922222
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-20 10:40:12 -0300

    feat: repository da entidade usuario.

---
+++ backend/src/main/java/br/socialhub/api/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.socialhub.api.repositories;
+
+import br.socialhub.api.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+}
