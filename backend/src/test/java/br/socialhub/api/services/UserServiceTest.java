8bc3e318ec6c30ca0e791c7465049252828b4960
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-20 14:11:57 -0300

    feat: testes unitario da camada de servico userservice.

---
+++ backend/src/test/java/br/socialhub/api/services/UserServiceTest.java
@@ -0,0 +1,47 @@
+package br.socialhub.api.services;
+
+import br.socialhub.api.exceptions.IdadeMinimaException;
+import br.socialhub.api.models.Usuario;
+import br.socialhub.api.repositories.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static br.socialhub.api.common.UserConstant.*;
+import static org.assertj.core.api.Assertions.*;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @Test
+    public void createUser_WithValidData_ReturnsUser(){
+        when(usuarioRepository.save(USUARIO)).thenReturn(USUARIO);
+        when(passwordEncoder.encode(SENHA_DEFAULT)).thenReturn(SENHA_DEFAULT);
+
+        Usuario sut = userService.createUser(USUARIO_CREATED);
+
+        assertThat(sut).isEqualTo(USUARIO);
+    }
+
+    @Test
+    public void createUser_WithInvalidData_ThrowsException(){
+        assertThatThrownBy(() -> userService.createUser(INVALID_USUARIO_CREATED))
+                .isInstanceOf(IdadeMinimaException.class) // Ou a exceção específica esperada
+                .hasMessageContaining("Idade mínima inválida: a idade permitida é a partir de 12 anos.");
+    }
+}
