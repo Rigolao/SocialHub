b0c222832c7e3d5644ad7d376e96c31008b4e4b0
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-20 11:01:21 -0300

    feat: controlador de exception

---
+++ backend/src/main/java/br/socialhub/api/handlers/GeneralExceptionHandler.java
@@ -0,0 +1,42 @@
+package br.socialhub.api.handlers;
+
+import br.socialhub.api.dtos.ErroDTO;
+import br.socialhub.api.exceptions.DocumentoInvalidoException;
+import br.socialhub.api.exceptions.IdadeMinimaException;
+import br.socialhub.api.exceptions.RecursoNaoEncontradoException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErroDTO> handleRecursoNaoEncontrado (RecursoNaoEncontradoException e){
+        ErroDTO erro = new ErroDTO(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErroDTO> handleIdadeMinima (IdadeMinimaException e){
+        ErroDTO erro = new ErroDTO(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(DocumentoInvalidoException.class)
+    public ResponseEntity<ErroDTO> handleDoncumentoInvalido (DocumentoInvalidoException e){
+        ErroDTO erro = new ErroDTO(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+}
