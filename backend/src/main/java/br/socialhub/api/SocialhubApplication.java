f5a3b15277a5a49c016bc94e3d1780f061a73826
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-20 10:53:37 -0300

    feat: habilitando o async

--- backend/src/main/java/br/socialhub/api/SocialhubApplication.java
+++ backend/src/main/java/br/socialhub/api/SocialhubApplication.java
@@ -2,8 +2,10 @@ package br.socialhub.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SocialhubApplication {
 
 	public static void main(String[] args) {
