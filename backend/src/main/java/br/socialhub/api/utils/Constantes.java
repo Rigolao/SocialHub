5c7381d8a905dc00465605821fff0820c573ce00
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-21 13:40:13 -0300

    feat: new constantes

--- backend/src/main/java/br/socialhub/api/utils/Constantes.java
+++ backend/src/main/java/br/socialhub/api/utils/Constantes.java
@@ -19,6 +19,24 @@ public interface Constantes {
     String RESOURCE_USER = "Usuário";
     String RESOURCE_EMAIL = "Email";
 
+    /**
+     * JWT AUTH
+     */
+
+    String BEARER = "Bearer ";
+
+    /**
+     * Header request
+     */
+
+    String AUTHORIZATION = "Authorization";
+
+    /**
+     *  Utilitários
+     */
+
+    String WHITESPACE = " ";
+
 
     /**
      * Links
@@ -37,6 +55,9 @@ public interface Constantes {
     String VALIDATION_REQUIRED_DOCUMENT_TYPE = "O campo tipo de documento é obrigatório.";
     String VALIDATION_REQUIRED_EMAIL = "O campo email é obrigatório.";
     String VALIDATION_REQUIRED_PASSWORD = "O campo senha é obrigatório.";
+    String VALIDATION_REQUIRED_NEW_PASSWORD = "O campo nova senha é obrigatório.";
+    String VALIDATION_REQUIRED_CONFIRM_PASSWORD = "O campo confirma senha é obrigatório.";
+    String VALIDATION_REQUIRED_ID = "É necessario passar o identificador do usuario";
 
 
     String VALIDATION_EMAIL = "O campo email precisa ser válido.";
@@ -57,6 +78,13 @@ public interface Constantes {
     String MESSAGE_SUCESS_UPLOAD_PHOTO = "Foto enviada com sucesso.";
 
     String MESSAGE_BAD_REQUEST_RESET = "Token inválido. Por favor, solicite um novo link para resetar a senha.";
+
+    /**
+     * Exception messages
+     */
+
+    String EXCEPTION_PASSWORD_MISMATCH = "As senhas não coincidem.";
+    String EXCEPTION_UNAUTHORIZED_ACESS_PASSWORD = "O usuário não tem permissão para atualizar esta senha.";
 
 
 }
