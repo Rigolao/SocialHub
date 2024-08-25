ab7f1b46cac2d76b111194809b3cbc18ea2c1d56
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-25 02:00:03 -0300

    feat(exception): adicionar novos handlers para exceções personalizadas
    Implementados novos handlers para tratamento de exceções personalizadas, incluindo ResourceNotFoundException, MinimumAgeException, e InvalidTokenException. Atualizada a resposta de erro para refletir as exceções específicas e melhorar a comunicação de falhas para o cliente.

--- backend/src/main/java/br/socialhub/api/handlers/GeneralExceptionHandler.java
+++ backend/src/main/java/br/socialhub/api/handlers/GeneralExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,7 +17,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+        StringBuilder errorMessages = new StringBuilder("Erros de validação: ");
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errorMessages.append(String.format("Campo '%s': %s;", fieldName, errorMessage));
+        });
+
+        // Criar o DTO de erro com as mensagens
+        ErroDTO erro = new ErroDTO(errorMessages.toString());
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(erro);
+
     }
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErroDTO> handleResourceNotFoundException (ResourceNotFoundException e){
@@ -27,19 +38,19 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(MinimumAgeException.class)
     public ResponseEntity<ErroDTO> handleMinimumAge (MinimumAgeException e){
         ErroDTO erro = new ErroDTO(e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
     }
 
     @ExceptionHandler(InvalidDocumentException.class)
     public ResponseEntity<ErroDTO> handleDoncumentoInvalido (InvalidDocumentException e){
         ErroDTO erro = new ErroDTO(e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
     public ResponseEntity<ErroDTO> handlePasswordMismatchException(PasswordMismatchException e){
         ErroDTO erro = new ErroDTO(e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
     }
 
     @ExceptionHandler(UnauthorizedAccessException.class)
@@ -51,14 +62,35 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DocumentNumberNotUniqueException.class)
     public ResponseEntity<ErroDTO> handleDocumentNumberNotUniqueException(DocumentNumberNotUniqueException e){
         ErroDTO erro = new ErroDTO(e.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(erro);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
     }
 
     @ExceptionHandler(EmailNotUniqueException.class)
     public ResponseEntity<ErroDTO> handleEmailNotUniqueException(EmailNotUniqueException e){
         ErroDTO erro = new ErroDTO(e.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(erro);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
     }
 
+    @ExceptionHandler(RestrictedRoleException.class)
+    public ResponseEntity<ErroDTO> handleRestrictedRoleException(RestrictedRoleException e){
+        ErroDTO erro = new ErroDTO(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);
+    }
 
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<ErroDTO> handleInvalidTokenException(InvalidTokenException e){
+        ErroDTO erro = new ErroDTO(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);
+    }
+    @ExceptionHandler(UserAlreadyAssignedToSpaceException.class)
+    public ResponseEntity<ErroDTO> handleInvalidTokenException(UserAlreadyAssignedToSpaceException e){
+        ErroDTO erro = new ErroDTO(e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+    }
+
+    @ExceptionHandler(UserNotFoundInSpaceException.class)
+    public ResponseEntity<ErroDTO> handleUserNotFoundInSpaceException(UserNotFoundInSpaceException e){
+        ErroDTO erro = new ErroDTO(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(erro);
+    }
 }
