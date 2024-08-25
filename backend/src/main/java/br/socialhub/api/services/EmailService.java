85cbb756c55e4ba133f3e423b9318ce96fa9f477
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-25 01:57:46 -0300

    refactor(email): melhorar o envio de e-mails e utilizar templates
    Refatorou os métodos de envio de e-mails para utilizar templates na geração de conteúdo. Introduziu um método privado para enviar e-mails em HTML, melhorando a reutilização do código e a manutenção. Aprimorou o logging das operações de envio de e-mails e atualizou os nomes dos métodos para maior clareza.

--- backend/src/main/java/br/socialhub/api/services/EmailService.java
+++ backend/src/main/java/br/socialhub/api/services/EmailService.java
@@ -2,19 +2,17 @@ package br.socialhub.api.services;
 
 import br.socialhub.api.templates.EmailTemplateLoader;
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
-
-import static br.socialhub.api.utils.Constantes.UTF_8;
-import static br.socialhub.api.utils.Constantes.WELCOME_SOCIAL_HUB;
+import static br.socialhub.api.utils.Constantes.*;
 
 @Async
 @RequiredArgsConstructor
@@ -24,34 +22,49 @@ public class EmailService {
     private final JavaMailSender mailSender;
     private final EmailTemplateLoader emailTemplateLoader;
 
-    public void sendPasswordResetEmail(final String email, final String resetLink){
-        String subject = "Password Reset Request";
-        String message = "You requested a password reset. Click the link below to reset your password:\n" + resetLink;
-
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setTo(email);
-        mailMessage.setSubject(subject);
-        mailMessage.setText(message);
-
-        mailSender.send(mailMessage);
+    public void sendPasswordResetEmail(final String email, final String resetLink) {
+        String content = emailTemplateLoader.loadResetPasswordTemplate(resetLink);
+        _sendHtmlEmail(email, RESET_PASSWORD_TITLE_EMAIL, content);
     }
 
     public void sendWelcomeEmail(final String email, final String name) {
         String content = emailTemplateLoader.loadWelcomeEmailTemplate(name);
+        _sendHtmlEmail(email, WELCOME_SOCIAL_HUB_TITLE_EMAIL, content);
+    }
 
+    public void sendInviteUser(String spaceName, String inviteeName, String inviteeEmail, String nameCreator, String link) {
+        String content = emailTemplateLoader.loadInviteSpaceTemplate(spaceName, inviteeName, nameCreator, link);
+        _sendHtmlEmail(inviteeEmail, INVITE_USER_TITLE_EMAIL, content);
+    }
+
+    private void _sendHtmlEmail(String recipientEmail, String subject, String htmlContent) {
         try {
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, UTF_8);
 
-            helper.setTo(email);
-            helper.setSubject(WELCOME_SOCIAL_HUB);
-            helper.setText(content, true);
+            helper.setTo(recipientEmail);
+            helper.setSubject(subject);
+
+            // Cria o multipart relacionado
+            MimeMultipart multipart = new MimeMultipart("related");
+
+            // Parte HTML
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(htmlContent, "text/html");
+            multipart.addBodyPart(messageBodyPart);
+
+            // Adiciona a imagem como parte inline, se necessário
+            // Neste caso, se você estiver apenas usando base64 no HTML, você pode pular esta parte.
+            // Se precisar de imagens adicionais inline, adicione-as aqui.
+
+            // Define o conteúdo do e-mail
+            message.setContent(multipart);
 
             mailSender.send(message);
-        } catch (MessagingException e){
-            log.warn("Email de boas vindas falhou para {}", email);
+            log.info("Email enviado com sucesso para {}", recipientEmail);
+        } catch (MessagingException e) {
+            log.warn("Falha ao enviar email para {}", recipientEmail);
         }
-        log.info("Email de boas vindas enviado com sucesso para {}", email);
     }
 
 }
