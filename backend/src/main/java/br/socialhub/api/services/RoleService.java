933b5673a40ee9337dbf48c402317d8de3b6c8a1
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-25 02:02:45 -0300

    refactor(role): melhorar a gestão de cargos e adicionar novos métodos
    - Renomeado método findByDescription para findRoleCreator e ajustado o método roleCreator para chamá-lo.
    - Adicionado método getAll para retornar todos os cargos exceto o CREATOR como RoleResponseDTO.
    - Adicionado método findById para buscar um cargo por ID e lançar ResourceNotFoundException se não encontrado.

--- backend/src/main/java/br/socialhub/api/services/RoleService.java
+++ backend/src/main/java/br/socialhub/api/services/RoleService.java
@@ -1,20 +1,38 @@
 package br.socialhub.api.services;
 
+import br.socialhub.api.dtos.role.RoleResponseDTO;
 import br.socialhub.api.enums.RoleType;
+import br.socialhub.api.exceptions.ResourceNotFoundException;
 import br.socialhub.api.models.Cargo;
 import br.socialhub.api.repositories.CargoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static br.socialhub.api.utils.Constantes.RESOURCE_ROLE;
+
 @RequiredArgsConstructor
 @Service
 public class RoleService {
     private final CargoRepository cargoRepository;
-    public Cargo findByDescription(final RoleType roleType){
-        return cargoRepository.findByDescription(roleType);
+    private Cargo findRoleCreator(){
+        return cargoRepository.findByDescription(RoleType.CREATOR);
     }
 
     public Cargo roleCreator(){
-        return findByDescription(RoleType.CREATOR);
+        return findRoleCreator();
     }
+
+    public List<RoleResponseDTO> getAll() {
+        return cargoRepository.findAll().stream()
+                .filter(role -> !role.getDescription().equals(RoleType.CREATOR))
+                .map(RoleResponseDTO::new).collect(Collectors.toList());
+    }
+
+    public Cargo findById(Long id){
+        return cargoRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(RESOURCE_ROLE));
+    }
+
 }
