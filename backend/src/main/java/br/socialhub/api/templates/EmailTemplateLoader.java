da5d6dff8e8760153158d2515051a5b4462962f7
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-25 01:59:10 -0300

    feat(template): adicionar suporte a novos templates de e-mail
    Adicionadas funcionalidades para carregar e substituir variáveis em templates de e-mail. Inclui novos métodos para carregar templates de convite para espaços e de redefinição de senha. Atualizado o método de template de boas-vindas para incluir link de exploração e ano atual.

--- backend/src/main/java/br/socialhub/api/templates/EmailTemplateLoader.java
+++ backend/src/main/java/br/socialhub/api/templates/EmailTemplateLoader.java
@@ -6,8 +6,9 @@ import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 
-import static br.socialhub.api.utils.Constantes.WELCOME_EMAIL_TEMPLATE_PATH;
+import static br.socialhub.api.utils.Constantes.*;
 
 @Component
 public class EmailTemplateLoader {
@@ -15,7 +16,34 @@ public class EmailTemplateLoader {
         try {
             ClassPathResource resource = new ClassPathResource(WELCOME_EMAIL_TEMPLATE_PATH);
             String content = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-            return content.replace("{{nome}}", name);
+            return content.replace("{{name}}", name)
+                    .replace("{{exploreLink}}", LINK_FRONT_END)
+                    .replace("{{year}}", String.valueOf(LocalDate.now().getYear()));
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar o template de e-mail", e);
+        }
+    }
+
+    public String loadInviteSpaceTemplate(final String nameSpace, final String name, final String inviteeName, final String link) {
+        try {
+            ClassPathResource resource = new ClassPathResource(INVITE_EMAIL_TEMPLATE_PATH);
+            String content = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+            return content.replace("{{name}}", name)
+                    .replace("{{inviterName}}", inviteeName)
+                    .replace("{{spaceName}}", nameSpace)
+                    .replace("{{invitationLink}}", link)
+                    .replace("{{logoImageBase64}}", LOGO_BASE64)
+                    .replace("{{year}}", String.valueOf(LocalDate.now().getYear()));
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar o template de e-mail", e);
+        }
+    }
+    public String loadResetPasswordTemplate(String link) {
+        try {
+            ClassPathResource resource = new ClassPathResource(RESET_EMAIL_TEMPLATE_PATH);
+            String content = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+            return content.replace("{{resetLink}}", link)
+                    .replace("{{year}}", String.valueOf(LocalDate.now().getYear()));
         } catch (IOException e) {
             throw new RuntimeException("Erro ao carregar o template de e-mail", e);
         }
