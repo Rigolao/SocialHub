0df783e390904aca137c0ca6372b3d5e6472b874
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-25 15:26:29 -0300

    feat: buscar por email by like

--- backend/src/main/java/br/socialhub/api/repositories/UsuarioRepository.java
+++ backend/src/main/java/br/socialhub/api/repositories/UsuarioRepository.java
@@ -4,6 +4,7 @@ import br.socialhub.api.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
 
     Optional<Usuario> findByDocumentNumber(String documentNumber);
+
+    Optional<List<Usuario>> findByEmailStartingWithIgnoreCase(String filter);
 }
