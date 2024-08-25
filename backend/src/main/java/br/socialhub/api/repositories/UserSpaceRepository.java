e919abbbbb1a6f75723bbd2b98d599441201e2e2
João Marques Oliveira <jomoliveira@unaerp.br>
2024-08-25 15:30:52 -0300

    feat: Adiciona métodos para buscar relacionamentos de usuário com espaço e cargo

--- backend/src/main/java/br/socialhub/api/repositories/UserSpaceRepository.java
+++ backend/src/main/java/br/socialhub/api/repositories/UserSpaceRepository.java
@@ -1,7 +1,15 @@
 package br.socialhub.api.repositories;
 
+import br.socialhub.api.models.Cargo;
+import br.socialhub.api.models.Space;
+import br.socialhub.api.models.Usuario;
 import br.socialhub.api.models.UsuarioSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserSpaceRepository extends JpaRepository<UsuarioSpace, Long> {
+    Optional<UsuarioSpace> findByUserAndSpaceAndRole(Usuario user, Space space, Cargo role);
+
+    Optional<UsuarioSpace> findByUserAndSpace(Usuario user, Space space);
 }
