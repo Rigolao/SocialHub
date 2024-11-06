b9b41a2e01787f791b8c00188c5b44ba8cc40aec
rigolao <mrigolao@gmail.com>
2024-11-06 19:27:21 -0300

    Feat: Selecionar primeiro space no qual usuário é criador.

--- frontend/src/components/custom/space-selector.tsx
+++ frontend/src/components/custom/space-selector.tsx
@@ -3,6 +3,7 @@ import LoadingSpinner from "@/components/ui/loding-spinner.tsx";
 import {useSpace} from "@/hooks/spaces/use-space.ts";
 import useGetUser from "@/hooks/user/use-get-user.ts";
 import {Space} from "@/types/spaces";
+import {useEffect} from "react";
 
 export default function SpaceSelector() {
 
@@ -14,6 +15,12 @@ export default function SpaceSelector() {
 
         !!space && setSelectedSpace(space);
     }
+
+    useEffect(() => {
+        if (!selectedSpace && user?.spaces.length) {
+            setSelectedSpace(user.spaces.filter(space => space.role === 'CREATOR')[0]);
+        }
+    }, [selectedSpace, setSelectedSpace, user?.spaces]);
 
     return (
         <>
