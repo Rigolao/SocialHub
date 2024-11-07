045afd96d8c26facd1d69b0e51a7b6411e2ff4bf
João Marques Oliveira <jomoliveira@unaerp.br>
2024-11-07 12:37:48 -0300

    refactor: ajustando o nome do atributo com letra minuscula;

--- backend/src/main/java/br/socialhub/api/dtos/post/PostDTO.java
+++ backend/src/main/java/br/socialhub/api/dtos/post/PostDTO.java
@@ -4,6 +4,6 @@ import br.socialhub.api.dtos.social_media.SocialMediaResponseDTO;
 
 import java.util.List;
 
-public record PostDTO (Long id, String title, List<SocialMediaResponseDTO> SocialNetwork){
+public record PostDTO (Long id, String title, List<SocialMediaResponseDTO> socialNetwork){
 
 }
